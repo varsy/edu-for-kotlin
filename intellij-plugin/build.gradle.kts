b5e78467b1a74f96db2b7c171dfe5e313ab91d5f
anastasia.birillo <anastasia.i.birillo@gmail.com>
2024-03-06 08:45:21 +0100

    ML4SE-A-33 AI Hints Initial Implementation
    Co-authored-by: anchouls <potryasaeva.anna@mail.ru>
    Co-authored-by: elizaveta <elizaveta.artser@jetbrains.com>
    Co-authored-by: Valerii <valerii.ovchinnikov@jetbrains.com>
    Co-authored-by: Maria.Tigina <maria.tigina@jetbrains.com>
    Co-authored-by: Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
    Co-authored-by: Mark Prudnikov <mark.prudnikov@jetbrains.com>

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -208,6 +208,8 @@ allprojects {
     implementationWithoutKotlin(rootProject.libs.retrofit)
     implementationWithoutKotlin(rootProject.libs.converter.jackson)
     implementationWithoutKotlin(rootProject.libs.kotlin.css.jvm)
+    implementationWithoutKotlin(rootProject.libs.educational.ml.library.core)
+    implementationWithoutKotlin(rootProject.libs.educational.ml.library.hints)
 
     testImplementation(rootProject.libs.junit)
     testImplementation(rootProject.libs.openTest4J)
@@ -1033,6 +1035,9 @@ fun <T : ModuleDependency> T.excludeKotlinDeps() {
   exclude(module = "kotlin-stdlib")
   exclude(module = "kotlin-stdlib-common")
   exclude(module = "kotlin-stdlib-jdk8")
+  exclude(module = "kotlin-stdlib-jdk7")
+  exclude(module = "kotlinx-coroutines-core")
+  exclude(module = "kotlinx-coroutines-core-jvm")
 }
 
 fun loadProperties(path: String): Properties {
