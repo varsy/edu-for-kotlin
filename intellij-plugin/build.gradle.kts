3a5a05d861b22f87e3662762cdbe7a5575861c46
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-09-03 14:39:12 +0000

    Renamed every occurrence of `description` to `prompt` and `draft` to `code`.

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -414,6 +414,14 @@ tasks {
     customRunIdeTask(DataSpell)
     customRunIdeTask(Rider, riderVersion)
   }
+
+  clean {
+    delete("generated-src")
+  }
+
+  compileTestKotlin {
+    dependsOn("generateTestGrammarSource")
+  }
 }
 
 /**
@@ -1074,6 +1082,12 @@ project("Edu-Jarvis") {
       arguments = listOf("-visitor", "-package", "com.jetbrains.edu.jarvis.grammar.generated")
       outputDirectory = file("src/com/jetbrains/edu/jarvis/grammar/generated")
       source = fileTree("src/main/antlr")
+    }
+
+    generateTestGrammarSource {
+      maxHeapSize = "128m"
+      arguments = listOf("-visitor", "-package", "com.jetbrains.edu.jarvis.grammar.generated")
+      outputDirectory = file("generated-src/antlr/test")
     }
 
     compileKotlin {
