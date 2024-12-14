1c183395b37d22ade9e5e220e5506068ca13c67f
Ilia Posov <ilia.posov@jetbrains.com>
2024-12-14 07:42:46 +0100

    Remove from PyHyperskill

--- intellij-plugin/Edu-Python/src/com/jetbrains/edu/python/learning/stepik/hyperskill/PyHyperskillBaseConfigurator.kt
+++ intellij-plugin/Edu-Python/src/com/jetbrains/edu/python/learning/stepik/hyperskill/PyHyperskillBaseConfigurator.kt
@@ -1,13 +1,7 @@
 package com.jetbrains.edu.python.learning.stepik.hyperskill
 
-import com.jetbrains.edu.learning.CourseInfoHolder
-import com.jetbrains.edu.learning.EduCourseBuilder
 import com.jetbrains.edu.learning.courseFormat.Course
-import com.jetbrains.edu.learning.newproject.CourseProjectGenerator
 import com.jetbrains.edu.python.learning.PyConfigurator
-import com.jetbrains.edu.python.learning.PyCourseBuilder
-import com.jetbrains.edu.python.learning.newproject.PyCourseProjectGenerator
-import com.jetbrains.edu.python.learning.newproject.PyProjectSettings
 
 /**
  * This class is needed as a hack to override behavior of base configurator during Hyperskill course creation
@@ -15,19 +9,4 @@ import com.jetbrains.edu.python.learning.newproject.PyProjectSettings
  */
 class PyHyperskillBaseConfigurator : PyConfigurator() {
   override fun getMockFileName(course: Course, text: String): String = MAIN_PY
-
-  override val courseBuilder: EduCourseBuilder<PyProjectSettings>
-    get() = PyHyperskillCourseBuilder()
-
-  private class PyHyperskillCourseBuilder : PyCourseBuilder() {
-    override fun getCourseProjectGenerator(course: Course): CourseProjectGenerator<PyProjectSettings> {
-      return GeneratorWithoutAdditionalFiles(this, course)
-    }
-  }
-
-  private class GeneratorWithoutAdditionalFiles(builder: PyCourseBuilder, course: Course) : PyCourseProjectGenerator(builder, course) {
-    override fun createAdditionalFiles(holder: CourseInfoHolder<Course>) {
-      // do nothing, independently of what could a base PyCourseProjectGenerator create
-    }
-  }
 }
