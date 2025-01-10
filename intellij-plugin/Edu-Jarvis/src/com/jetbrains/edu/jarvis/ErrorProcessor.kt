JetBrains/educational-plugin

Implemented function recognition from context.

intellij-plugin/Edu-Jarvis/src/ErrorProcessor.kt
@@ -1,0 +1,15 @@
+import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
+
+/**
+ * Returns an [[AnnotatorParametrizedError]] associated with relevant context.
+ */
+interface ErrorProcessor {
+  /**
+   * Processes the case with isolated code blocks.
+   */
+  fun processIsolatedCode(): AnnotatorParametrizedError
+  /**
+   * Processes the case with function calls without braces.
+   */
+  fun processNoBracesFunctionCall(): AnnotatorParametrizedError
+}

intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtErrorProcessor.kt
@@ -1,0 +1,71 @@
+package com.jetbrains.edu.kotlin
+
+import ErrorProcessor
+import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator
+import com.jetbrains.edu.jarvis.enums.AnnotatorError
+import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
+import com.jetbrains.edu.jarvis.models.FunctionCall
+import com.jetbrains.edu.kotlin.jarvis.utils.ARGUMENT_SEPARATOR
+import com.jetbrains.edu.kotlin.jarvis.utils.CLOSE_PARENTHESIS
+import com.jetbrains.edu.kotlin.jarvis.utils.OPEN_PARENTHESIS
+
+class KtErrorProcessor(private val context: String, private val visibleFunctions: Collection<FunctionCall>) : ErrorProcessor {
+  override fun processIsolatedCode(): AnnotatorParametrizedError {
+    return when {
+      !context.isAFunctionCall() -> AnnotatorParametrizedError.NO_ERROR
+      else -> {
+        val functionCall = context.toFunctionCall()
+        when {
+          visibleFunctions.none { it.name == functionCall.name } ->
+            AnnotatorParametrizedError(
+              AnnotatorError.UNKNOWN_FUNCTION,
+              arrayOf(functionCall.name)
+            )
+          functionCall !in visibleFunctions ->
+            AnnotatorParametrizedError(
+              AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
+              arrayOf(functionCall.name, functionCall.numberOfArguments)
+            )
+          else -> AnnotatorParametrizedError.NO_ERROR
+        }
+      }
+    }
+  }
+
+  override fun processNoBracesFunctionCall(): AnnotatorParametrizedError {
+    val functionCall = context.toFunctionCall()
+    return when {
+      visibleFunctions.none { it.name == functionCall.name } ->
+        AnnotatorParametrizedError(
+          AnnotatorError.UNKNOWN_FUNCTION,
+          arrayOf(functionCall.name)
+        )
+      functionCall !in visibleFunctions ->
+        AnnotatorParametrizedError(
+          AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
+          arrayOf(functionCall.name, functionCall.numberOfArguments)
+        )
+      else -> AnnotatorParametrizedError.NO_ERROR
+    }
+  }
+
+  private fun String.toFunctionCall(): FunctionCall {
+    val functionName = this.substringBefore(OPEN_PARENTHESIS)
+    val parameters = if (OPEN_PARENTHESIS in this) {
+      this
+        .substringAfter(OPEN_PARENTHESIS)
+        .substringBefore(CLOSE_PARENTHESIS)
+    }
+    else {
+      ""
+    }
+    val numberOfParameters = if (parameters.isNotBlank()) {
+      parameters.count { it == ARGUMENT_SEPARATOR } + 1
+    }
+    else 0
+    return FunctionCall(functionName, numberOfParameters)
+  }
+
+  private fun String.isAFunctionCall() =
+    DescriptionErrorAnnotator.functionCallRegex.matches(this)
+}

intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionErrorAnnotator.kt
@@ -4,13 +4,11 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.jarvis.DescriptionAnnotatorResult
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator
-import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.codeBlockRegex
-import com.jetbrains.edu.jarvis.errors.AnnotatorError
+import com.jetbrains.edu.jarvis.enums.AnnotatorRule
+import com.jetbrains.edu.jarvis.enums.AnnotatorError
 import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
 import com.jetbrains.edu.jarvis.models.FunctionCall
-import com.jetbrains.edu.kotlin.jarvis.utils.ARGUMENT_SEPARATOR
-import com.jetbrains.edu.kotlin.jarvis.utils.CLOSE_PARENTHESIS
-import com.jetbrains.edu.kotlin.jarvis.utils.OPEN_PARENTHESIS
+import com.jetbrains.edu.kotlin.KtErrorProcessor
 import com.jetbrains.edu.kotlin.jarvis.utils.isDescriptionBlock
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtParameterList
@@ -29,68 +27,45 @@ class KtDescriptionErrorAnnotator : DescriptionErrorAnnotator {
 
   override fun PsiElement.isRelevant() = isDescriptionBlock()
 
-  override fun getIncorrectParts(context: PsiElement): Sequence<DescriptionAnnotatorResult> {
+  override fun getIncorrectParts(context: PsiElement): Collection<DescriptionAnnotatorResult> {
     val visibleFunctions = context.containingFile
       .getChildrenOfType<KtNamedFunction>()
       .mapNotNull { it.toFunctionCallOrNull() }
-    return codeBlockRegex
-      .findAll(context.text)
-      .mapNotNull { it.groups[1] }
-      .map {
-        DescriptionAnnotatorResult(
-          it.range,
-          it.value.getError(visibleFunctions)
-        )
+    return AnnotatorRule.values()
+      .flatMap { rule ->
+        getIncorrectPartsByRegex(context, rule.regex).map {
+          DescriptionAnnotatorResult(
+            it.range,
+            it.value.getError(rule, visibleFunctions)
+          )
+        }
       }
+      .toSet()
       .filter { it.parametrizedError.errorType != AnnotatorError.NONE }
+
   }
 
+  private fun getIncorrectPartsByRegex(context: PsiElement, regex: Regex): Sequence<MatchGroup> {
+    return regex
+      .findAll(context.text)
+      .mapNotNull { it.groups[1] }
+  }
 
   private fun KtNamedFunction.toFunctionCallOrNull(): FunctionCall? {
     val numberOfParameters = getChildOfType<KtParameterList>()?.parameters?.size ?: 0
     return FunctionCall(name ?: return null, numberOfParameters)
   }
 
-  private fun String.isAFunctionCall() =
-    // TODO: check if string is a function call from surrounding context
-    DescriptionErrorAnnotator.functionCallRegex.matches(this)
-
-  private fun String.toFunctionCall(): FunctionCall {
-    val functionName = this.substringBefore(OPEN_PARENTHESIS)
-    val parameters = this
-      .substringAfter(OPEN_PARENTHESIS)
-      .substringBefore(CLOSE_PARENTHESIS)
-
-    val numberOfParameters = if (parameters.isNotBlank()) {
-      parameters.count { it == ARGUMENT_SEPARATOR } + 1
-    }
-    else 0
-
-    return FunctionCall(functionName, numberOfParameters)
-  }
-
-  private fun String.getError(visibleFunctions: Collection<FunctionCall>): AnnotatorParametrizedError {
-    when {
-      this.isAFunctionCall() -> {
-        val functionCall = this.toFunctionCall()
-        if (visibleFunctions.find { it.name == functionCall.name } == null) {
-          return AnnotatorParametrizedError(
-            AnnotatorError.UNKNOWN_FUNCTION,
-            arrayOf(functionCall.name)
-          )
-        }
-        else if (functionCall !in visibleFunctions) {
-          return AnnotatorParametrizedError(
-            AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
-            arrayOf(functionCall.name, functionCall.numberOfArguments)
-          )
-        }
+  private fun String.getError(rule: AnnotatorRule, visibleFunctions: Collection<FunctionCall>): AnnotatorParametrizedError {
+    val processor = KtErrorProcessor(this, visibleFunctions)
+    return when(rule) {
+      AnnotatorRule.ISOLATED_CODE -> {
+        processor.processIsolatedCode()
       }
+      AnnotatorRule.NO_BRACES_FUNCTION_CALL -> {
+        processor.processNoBracesFunctionCall()
+      }
+      else -> AnnotatorParametrizedError.NO_ERROR
     }
-    return AnnotatorParametrizedError(
-      AnnotatorError.NONE,
-      emptyArray()
-    )
-
   }
 }

varsy/edu-for-kotlin

intellij-plugin/Edu-Jarvis/src/com/jetbrains/edu/jarvis/ErrorProcessor.kt
@@ -1,0 +1,17 @@
+package com.jetbrains.edu.jarvis
+
+import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
+
+/**
+ * Returns an [[AnnotatorParametrizedError]] associated with relevant context.
+ */
+interface ErrorProcessor {
+  /**
+   * Processes the case with isolated code blocks.
+   */
+  fun processIsolatedCode(): AnnotatorParametrizedError
+  /**
+   * Processes the case with function calls without parentheses.
+   */
+  fun processNoParenthesesFunctionCall(): AnnotatorParametrizedError
+}

intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/KtErrorProcessor.kt
@@ -1,0 +1,71 @@
+package com.jetbrains.edu.kotlin
+
+import com.jetbrains.edu.jarvis.ErrorProcessor
+import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator
+import com.jetbrains.edu.jarvis.enums.AnnotatorError
+import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
+import com.jetbrains.edu.jarvis.models.FunctionCall
+import com.jetbrains.edu.kotlin.jarvis.utils.ARGUMENT_SEPARATOR
+import com.jetbrains.edu.kotlin.jarvis.utils.CLOSE_PARENTHESIS
+import com.jetbrains.edu.kotlin.jarvis.utils.OPEN_PARENTHESIS
+
+class KtErrorProcessor(private val context: String, private val visibleFunctions: Collection<FunctionCall>) : ErrorProcessor {
+  override fun processIsolatedCode(): AnnotatorParametrizedError {
+    return when {
+      !context.isAFunctionCall() -> AnnotatorParametrizedError.NO_ERROR
+      else -> {
+        val functionCall = context.toFunctionCall()
+        when {
+          visibleFunctions.none { it.name == functionCall.name } ->
+            AnnotatorParametrizedError(
+              AnnotatorError.UNKNOWN_FUNCTION,
+              arrayOf(functionCall.name)
+            )
+          functionCall !in visibleFunctions ->
+            AnnotatorParametrizedError(
+              AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
+              arrayOf(functionCall.name, functionCall.numberOfArguments)
+            )
+          else -> AnnotatorParametrizedError.NO_ERROR
+        }
+      }
+    }
+  }
+
+  override fun processNoParenthesesFunctionCall(): AnnotatorParametrizedError {
+    val functionCall = context.toFunctionCall()
+    return when {
+      visibleFunctions.none { it.name == functionCall.name } ->
+        AnnotatorParametrizedError(
+          AnnotatorError.UNKNOWN_FUNCTION,
+          arrayOf(functionCall.name)
+        )
+      functionCall !in visibleFunctions ->
+        AnnotatorParametrizedError(
+          AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
+          arrayOf(functionCall.name, functionCall.numberOfArguments)
+        )
+      else -> AnnotatorParametrizedError.NO_ERROR
+    }
+  }
+
+  private fun String.toFunctionCall(): FunctionCall {
+    val functionName = this.substringBefore(OPEN_PARENTHESIS)
+    val parameters = if (OPEN_PARENTHESIS in this) {
+      this
+        .substringAfter(OPEN_PARENTHESIS)
+        .substringBefore(CLOSE_PARENTHESIS)
+    }
+    else {
+      ""
+    }
+    val numberOfParameters = if (parameters.isNotBlank()) {
+      parameters.count { it == ARGUMENT_SEPARATOR } + 1
+    }
+    else 0
+    return FunctionCall(functionName, numberOfParameters)
+  }
+
+  private fun String.isAFunctionCall() =
+    DescriptionErrorAnnotator.functionCallRegex.matches(this)
+}

intellij-plugin/Edu-Kotlin/src/com/jetbrains/edu/kotlin/jarvis/psi/KtDescriptionErrorAnnotator.kt
@@ -4,13 +4,11 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.psi.PsiElement
 import com.jetbrains.edu.jarvis.DescriptionAnnotatorResult
 import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator
-import com.jetbrains.edu.jarvis.DescriptionErrorAnnotator.Companion.codeBlockRegex
-import com.jetbrains.edu.jarvis.errors.AnnotatorError
+import com.jetbrains.edu.jarvis.enums.AnnotatorRule
+import com.jetbrains.edu.jarvis.enums.AnnotatorError
 import com.jetbrains.edu.jarvis.errors.AnnotatorParametrizedError
 import com.jetbrains.edu.jarvis.models.FunctionCall
-import com.jetbrains.edu.kotlin.jarvis.utils.ARGUMENT_SEPARATOR
-import com.jetbrains.edu.kotlin.jarvis.utils.CLOSE_PARENTHESIS
-import com.jetbrains.edu.kotlin.jarvis.utils.OPEN_PARENTHESIS
+import com.jetbrains.edu.kotlin.KtErrorProcessor
 import com.jetbrains.edu.kotlin.jarvis.utils.isDescriptionBlock
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtParameterList
@@ -29,68 +27,45 @@ class KtDescriptionErrorAnnotator : DescriptionErrorAnnotator {
 
   override fun PsiElement.isRelevant() = isDescriptionBlock()
 
-  override fun getIncorrectParts(context: PsiElement): Sequence<DescriptionAnnotatorResult> {
+  override fun getIncorrectParts(context: PsiElement): Collection<DescriptionAnnotatorResult> {
     val visibleFunctions = context.containingFile
       .getChildrenOfType<KtNamedFunction>()
       .mapNotNull { it.toFunctionCallOrNull() }
-    return codeBlockRegex
-      .findAll(context.text)
-      .mapNotNull { it.groups[1] }
-      .map {
-        DescriptionAnnotatorResult(
-          it.range,
-          it.value.getError(visibleFunctions)
-        )
+    return AnnotatorRule.values()
+      .flatMap { rule ->
+        getIncorrectPartsByRegex(context, rule.regex).map {
+          DescriptionAnnotatorResult(
+            it.range,
+            it.value.getError(rule, visibleFunctions)
+          )
+        }
       }
+      .toSet()
       .filter { it.parametrizedError.errorType != AnnotatorError.NONE }
+
   }
 
+  private fun getIncorrectPartsByRegex(context: PsiElement, regex: Regex): Sequence<MatchGroup> {
+    return regex
+      .findAll(context.text)
+      .mapNotNull { it.groups[1] }
+  }
 
   private fun KtNamedFunction.toFunctionCallOrNull(): FunctionCall? {
     val numberOfParameters = getChildOfType<KtParameterList>()?.parameters?.size ?: 0
     return FunctionCall(name ?: return null, numberOfParameters)
   }
 
-  private fun String.isAFunctionCall() =
-    // TODO: check if string is a function call from surrounding context
-    DescriptionErrorAnnotator.functionCallRegex.matches(this)
-
-  private fun String.toFunctionCall(): FunctionCall {
-    val functionName = this.substringBefore(OPEN_PARENTHESIS)
-    val parameters = this
-      .substringAfter(OPEN_PARENTHESIS)
-      .substringBefore(CLOSE_PARENTHESIS)
-
-    val numberOfParameters = if (parameters.isNotBlank()) {
-      parameters.count { it == ARGUMENT_SEPARATOR } + 1
-    }
-    else 0
-
-    return FunctionCall(functionName, numberOfParameters)
-  }
-
-  private fun String.getError(visibleFunctions: Collection<FunctionCall>): AnnotatorParametrizedError {
-    when {
-      this.isAFunctionCall() -> {
-        val functionCall = this.toFunctionCall()
-        if (visibleFunctions.find { it.name == functionCall.name } == null) {
-          return AnnotatorParametrizedError(
-            AnnotatorError.UNKNOWN_FUNCTION,
-            arrayOf(functionCall.name)
-          )
-        }
-        else if (functionCall !in visibleFunctions) {
-          return AnnotatorParametrizedError(
-            AnnotatorError.WRONG_NUMBER_OF_ARGUMENTS,
-            arrayOf(functionCall.name, functionCall.numberOfArguments)
-          )
-        }
+  private fun String.getError(rule: AnnotatorRule, visibleFunctions: Collection<FunctionCall>): AnnotatorParametrizedError {
+    val processor = KtErrorProcessor(this, visibleFunctions)
+    return when(rule) {
+      AnnotatorRule.ISOLATED_CODE -> {
+        processor.processIsolatedCode()
       }
+      AnnotatorRule.NO_PARENTHESES_FUNCTION_CALL -> {
+        processor.processNoParenthesesFunctionCall()
+      }
+      else -> AnnotatorParametrizedError.NO_ERROR
     }
-    return AnnotatorParametrizedError(
-      AnnotatorError.NONE,
-      emptyArray()
-    )
-
   }
 }