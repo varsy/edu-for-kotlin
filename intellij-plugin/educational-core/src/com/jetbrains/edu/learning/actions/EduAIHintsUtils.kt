3c83295a547d014bfa6485bb7884bea55303bfb9
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2025-01-29 19:52:01 +0100

    EDU-7696: Replace `isGetHintAvailable` With Action's Presentation Check

--- intellij-plugin/educational-core/src/com/jetbrains/edu/learning/actions/EduAIHintsUtils.kt
+++ intellij-plugin/educational-core/src/com/jetbrains/edu/learning/actions/EduAIHintsUtils.kt
@@ -1,16 +1,16 @@
 package com.jetbrains.edu.learning.actions
 
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.ex.ActionUtil
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.jetbrains.edu.learning.EduExperimentalFeatures
-import com.jetbrains.edu.learning.agreement.UserAgreementSettings
-import com.jetbrains.edu.learning.courseFormat.CheckStatus
-import com.jetbrains.edu.learning.courseFormat.EduCourse
-import com.jetbrains.edu.learning.courseFormat.EduFormatNames
-import com.jetbrains.edu.learning.courseFormat.tasks.EduTask
-import com.jetbrains.edu.learning.courseFormat.tasks.Task
-import com.jetbrains.edu.learning.isFeatureEnabled
+import com.jetbrains.edu.learning.taskToolWindow.ui.check.CheckPanel.Companion.ACTION_PLACE
 import org.jetbrains.annotations.NonNls
 
 /**
@@ -24,18 +24,23 @@ object EduAIHintsUtils {
   const val GET_HINT_ACTION_ID: String = "Educational.Hints.GetHint"
 
   /**
-   * Temporary solution to check if the action is available for current task.
-   * Will be removed as soon as proper checking of the existence of the corresponding function has been implemented.
-   * Such check should be done via verifying the existence of the required EPs.
+   * Temporary solution because currently [com.jetbrains.edu.aiHints.core.action.GetHint]'s presentation is not propagated to corresponding
+   * button in the [com.jetbrains.edu.learning.taskToolWindow.ui.check.CheckDetailsPanel].
    *
-   * @see [com.jetbrains.edu.aiHints.core.action.GetHint]
+   * @see <a href="https://youtrack.jetbrains.com/issue/EDU-7584">EDU-7584</a>
    */
-  fun isGetHintAvailable(task: Task): Boolean {
-    if (!isFeatureEnabled(EduExperimentalFeatures.AI_HINTS) || !UserAgreementSettings.getInstance().aiServiceAgreement) return false
-    val course = task.course as? EduCourse ?: return false
-    val isMarketplaceCourse = course.isStudy && course.isMarketplaceRemote
-    val isRequiredLanguage = course.languageId == EduFormatNames.KOTLIN || course.languageId == EduFormatNames.PYTHON
-    return isRequiredLanguage && isMarketplaceCourse && task is EduTask && task.status == CheckStatus.Failed
+  fun getHintActionPresentation(project: Project): Presentation {
+    val action = ActionManager.getInstance().getAction(GET_HINT_ACTION_ID) as ActionWithProgressIcon
+    // BACKCOMPAT: 2024.2 Replace with [AnActionEvent.createEvent]
+    @Suppress("DEPRECATION", "removal")
+    val anActionEvent = AnActionEvent.createFromInputEvent(
+      null,
+      ACTION_PLACE,
+      action.templatePresentation.clone(),
+      SimpleDataContext.builder().add(CommonDataKeys.PROJECT, project).build()
+    )
+    runReadAction { ActionUtil.performDumbAwareUpdate(action, anActionEvent, false) }
+    return anActionEvent.presentation
   }
 
   /**
